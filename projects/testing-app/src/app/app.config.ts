681199710e7fe3f091b062d9739138a9f4320743
Adel Soliman Ahmed <aabdelda@cisco.com>
2025-12-15 07:27:17 +0200

    update for publishing to npm and running the test app from there

--- projects/testing-app/src/app/app.config.ts
+++ projects/testing-app/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgxTimeSchedulerService } from '@adelsoli/ngx-scheduler';
 
 import { routes } from './app.routes';
 import { MockHttpInterceptor } from './mock-http.interceptor';
@@ -9,6 +10,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
-    { provide: HTTP_INTERCEPTORS, useClass: MockHttpInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: MockHttpInterceptor, multi: true },
+    NgxTimeSchedulerService
   ]
 };
